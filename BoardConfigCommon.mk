9ab78d6527b6896beb86a357a4eb87f0db049ff6
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:39:22 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -256,3 +256,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 -include vendor/sony/sm8350-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
